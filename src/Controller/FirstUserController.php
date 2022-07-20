937936a18a7ba2f85fadca6d24765655fb604fde
tennessee <houry9@gmail.com>
2022-07-20 16:09:24 +0200

    hash password

--- src/Controller/FirstUserController.php
+++ src/Controller/FirstUserController.php
@@ -6,15 +6,17 @@ use App\Form\RegistrationType;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class FirstUserController extends AbstractController
 {
     #[Route('/first/user', name: 'app_first_user')]
-    public function index(Request $request, EntityManagerInterface $em, UserRepository $userRepository): Response
+    public function index(Request $request, EntityManagerInterface $em, UserRepository $userRepository,  UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         // If no user exists, create an admin user
         if (count($userRepository->findAll()) === 0) {
@@ -24,6 +26,8 @@ class FirstUserController extends AbstractController
 
             if ($form->isSubmitted() && $form->isValid()) {
                 $user->setRoles(['ROLE_ADMIN']);
+                $password = $userPasswordHasherInterface->hashPassword($user, $user->getPassword());
+                $user->setPassword($password);
                 $em->persist($user);
                 $em->flush();
                 return $this->redirectToRoute('app_home');
