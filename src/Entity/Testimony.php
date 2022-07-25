e7baae2488c032d3bdffd055981338465e570bb4
tennessee <houry9@gmail.com>
2022-07-25 14:30:14 +0200

    Configure crud easy admin

--- src/Entity/Testimony.php
+++ src/Entity/Testimony.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use App\Repository\TestimonyRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TestimonyRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Testimony
 {
     #[ORM\Id]
@@ -15,7 +17,8 @@ class Testimony
     #[ORM\Column()]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255),
+    #[
+        ORM\Column(length: 255),
         Assert\NotBlank,
         Assert\Length(
             min: 2,
@@ -26,27 +29,45 @@ class Testimony
     ]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255),
-    Assert\Email(
-        message: "L'email {{ value }} n'est pas valide"
-    ),
-    Assert\NotBlank]
+    #[
+        ORM\Column(length: 255),
+        Assert\Email(
+            message: "L'email {{ value }} n'est pas valide"
+        ),
+        Assert\NotBlank
+    ]
     private ?string $email = null;
 
-    #[ORM\Column,
-    Assert\Range(
-        min: 0,
-        max: 5,
-        notInRangeMessage: "Votre note doit être comprise entre {{ min }} et {{ max }}. Vous avez noté {{ value }}.",
-    ),
-    Assert\NotBlank,
+    #[
+        ORM\Column,
+        Assert\Range(
+            min: 0,
+            max: 5,
+            notInRangeMessage: "Votre note doit être comprise entre {{ min }} et {{ max }}. Vous avez noté {{ value }}.",
+        ),
+        Assert\NotBlank,
     ]
     private ?int $rate = null;
 
-    #[ORM\Column(type: Types::TEXT),
-    Assert\NotBlank,
+    #[
+        ORM\Column(type: Types::TEXT),
+        Assert\NotBlank,
     ]
     private ?string $comment = null;
+
+    #[ORM\Column]
+    private ?bool $isVisible = false;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
 
     public function getId(): ?int
     {
@@ -98,6 +119,28 @@ class Testimony
     {
         $this->comment = $comment;
 
+        return $this;
+    }
+
+    public function isIsVisible(): ?bool
+    {
+        return $this->isVisible;
+    }
+
+    public function setIsVisible(bool $isVisible): self
+    {
+        $this->isVisible = $isVisible;
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
         return $this;
     }
 }
