471b84c270f13033223f8184b79ed796222a32a7
tennessee <houry9@gmail.com>
2022-07-25 17:24:20 +0200

    refactore js

---
+++ assets/scripts/app-testimony.js
@@ -0,0 +1,23 @@
+['touchstart', 'mouseover'].forEach(function (eventName) {
+    document.getElementById('starContainer').addEventListener(
+        eventName,
+        function (e) {
+            if (e.target.classList.contains('star')) {
+                document.getElementById('testimony_rate').value =
+                    e.target.dataset.value;
+                document
+                    .getElementById('starContainer')
+                    .querySelectorAll('.star')
+                    .forEach(function (star, index) {
+                        if (index + 1 <= e.target.dataset.value) {
+                            star.classList.add('fas');
+                            star.classList.remove('far');
+                        } else {
+                            star.classList.add('far');
+                            star.classList.remove('fas');
+                        }
+                    });
+            }
+        },
+        false
+    )});
