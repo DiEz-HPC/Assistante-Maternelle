987503ec595d2bb7a205e4e7cbdb6efe8521540e
tennessee <houry9@gmail.com>
2022-07-25 14:30:14 +0200

    Configure crud easy admin

--- src/Controller/Admin/TestimonyCrudController.php
+++ src/Controller/Admin/TestimonyCrudController.php
@@ -3,7 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Testimony;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TestimonyCrudController extends AbstractCrudController
 {
@@ -12,14 +21,49 @@ class TestimonyCrudController extends AbstractCrudController
         return Testimony::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new(propertyName: 'name', label: 'Nom'),
+            EmailField::new(propertyName: 'email', label: 'Email'),
+            NumberField::new(propertyName: 'rate', label: 'Note'),
+            TextField::new(propertyName: 'comment', label: 'Commentaire'),
+            DateField::new(propertyName: 'createdAt', label: 'Date du commentaire')->onlyOnDetail(),
+            BooleanField::new(propertyName: 'isVisible', label: 'Publié'),
         ];
     }
-    */
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+                return $action->setLabel('Supprimer');
+            })
+            ->update(Crud::PAGE_INDEX, Action::BATCH_DELETE, function (Action $action) {
+                return $action->setLabel('Supprimer');
+            })
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+                return $action->setLabel('Détails');
+            })
+
+            ->remove(Crud::PAGE_DETAIL, Action::EDIT)
+            ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $action) {
+                return $action->setLabel('Supprimer');
+            })
+            ->update(Crud::PAGE_DETAIL, Action::INDEX, function (Action $action) {
+                return $action->setLabel('Retour à la liste');
+            });
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Gestion des avis clients')
+            ->setPageTitle(Crud::PAGE_INDEX, '%entity_label_singular%')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Modifier le compte');
+    }
+    
+   
 }
